2a0a45cfbe1d7ccdcea595a24539e1232fb364d6
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-10-09 23:50:11 +0300

    Pages

---
+++ FoodieGuide/uı/view/FoodPage.swift
@@ -0,0 +1,29 @@
+//
+//  FoodPage.swift
+//  FoodieGuide
+//
+//  Created by Gül Karataş on 9.10.2024.
+//
+
+import UIKit
+
+class FoodPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
