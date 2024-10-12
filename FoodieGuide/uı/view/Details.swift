456885630163b900386379e92d45de4759dfc24e
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-10-12 23:03:42 +0300

    DetailsPage

--- FoodieGuide/uı/view/Details.swift
+++ FoodieGuide/uı/view/Details.swift
@@ -1,37 +1,31 @@
-//
-//  Details.swift
-//  FoodieGuide
-//
-//  Created by Gül Karataş on 11.10.2024.
-//
-
 import UIKit
 
 class Details: UIViewController {
 
-    var foodList : Food?
+    var foodList: Food?
     
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var foodImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Food nesnesinden alınan bilgileri ilgili UI öğelerine atıyoruz
         if let f = foodList {
-            foodImageView.image = UIImage(named: f.image!)
-            numberLabel.text = String(f.id!)
-            priceLabel.text = String(f.price!)
-            nameLabel.text = f.name
+            foodImageView.image = UIImage(named: f.image!) // Resmi ayarla
+            numberLabel.text = String(f.id!)               // ID'yi göster
+            priceLabel.text = "\(f.price!) TL"             // Fiyatı göster
+            nameLabel.text = f.name                         // Yiyecek adını göster
         }
-        
     }
     
-
-   
     @IBAction func azaltButton(_ sender: Any) {
+        // Azaltma işlemini burada tanımlayabilirsin
     }
     
     @IBAction func arttırButton(_ sender: Any) {
+        // Arttırma işlemini burada tanımlayabilirsin
     }
 }
--- FoodieGuide/uı/view/FoodPage.swift
+++ FoodieGuide/uı/view/FoodPage.swift
@@ -2,55 +2,48 @@ import UIKit
 import Kingfisher
 
 class FoodPage: UIViewController {
-    
+
     @IBOutlet weak var foodCollectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
+
     var viewModel = FoodPageViewModel()
-    
+
     var foodList = [Food]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         foodCollectionView.delegate = self
         foodCollectionView.dataSource = self
-        
+
         view.backgroundColor = UIColor(named: "BackgroundColor")
         navigationItem.hidesBackButton = true
         customizeTabBar()
         navigationTitle()
-       
+
+        // Örnek veri ekleme
         let f = Food(id:1 ,name: "Pizza", image: "ottoman", price: 100)
         let f1 = Food(id:2 ,name: "Burger", image: "parfüm", price: 200)
         let f3 = Food(id:3 ,name: "Pasta", image: "saat", price: 300)
         let f4 = Food(id:4 ,name: "Salad", image: "telefon", price: 400)
-        
+
         foodList.append(f)
         foodList.append(f1)
         foodList.append(f3)
         foodList.append(f4)
-        
+
+        // Koleksiyon görünümünün tasarımını ayarlama
         let design = UICollectionViewFlowLayout()
-
-        design.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
-
-        
-        design.minimumInteritemSpacing = 10
+        design.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        design.minimumInteritemSpacing = 1
         design.minimumLineSpacing = 10
 
-       
         let screenWidth = UIScreen.main.bounds.width
-
-       
-        let itemWidth = (screenWidth - 50) / 3
+        let itemWidth = (screenWidth - 30) / 2
 
         design.itemSize = CGSize(width: itemWidth, height: itemWidth * 1.6)
         foodCollectionView.collectionViewLayout = design
-
-        
     }
-    
-    
+
     func navigationTitle() {
         self.navigationItem.title = "Foodie"
         let color = UINavigationBarAppearance()
@@ -58,84 +51,98 @@ class FoodPage: UIViewController {
         color.backgroundColor = UIColor(named: "TextColor")
         color.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationController?.navigationBar.barStyle = .black
-        
+
         navigationController?.navigationBar.standardAppearance = color
         navigationController?.navigationBar.compactAppearance = color
         navigationController?.navigationBar.scrollEdgeAppearance = color
     }
-    
+
     private func customizeTabBar() {
         guard let tabBar = tabBarController?.tabBar else { return }
 
-        
         let appearance = UITabBarAppearance()
         appearance.backgroundColor = UIColor.white
         appearance.shadowColor = UIColor.lightGray
         appearance.shadowImage = UIImage()
 
-       
         let itemAppearance = UITabBarItemAppearance()
         configureItemAppearance(itemAppearance)
-        
-        
+
         appearance.stackedLayoutAppearance = itemAppearance
         appearance.inlineLayoutAppearance = itemAppearance
-        
-        
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundEffect = UIBlurEffect(style: .light) // Bulanık arka plan
 
-        
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .light)
+
         tabBar.standardAppearance = appearance
         tabBar.scrollEdgeAppearance = appearance
-        
+
         tabBar.layer.cornerRadius = 20
         tabBar.clipsToBounds = true
     }
-    
+
     private func configureItemAppearance(_ itemAppearance: UITabBarItemAppearance) {
-        
         itemAppearance.selected.iconColor = UIColor(named: "TextColor") ?? UIColor.black
-        
         itemAppearance.normal.iconColor = UIColor.systemGray
-        
     }
 }
 
-extension FoodPage: UICollectionViewDataSource, UICollectionViewDelegate , CellProtocol{
-   
-    
+extension FoodPage: UICollectionViewDataSource, UICollectionViewDelegate, CellProtocol {
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return foodList.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let food = foodList[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! FoodCell
-        
+
         cell.foodImageView.image = UIImage(named: food.image!)
         cell.foodLabel.text = food.name
         cell.priceLabel.text = "\(food.price!)"
-        
+
+        // Hücre için kenarlık ve köşe yuvarlama
         cell.layer.borderColor = UIColor.gray.cgColor
         cell.layer.borderWidth = 0.5
         cell.layer.cornerRadius = 10.0
-        
+
+        // Resime tıklanabilirlik ekleme
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
+        cell.foodImageView.isUserInteractionEnabled = true
+        cell.foodImageView.addGestureRecognizer(tapGesture)
+        tapGesture.view?.tag = indexPath.row  // Tıklanan resmin sırasını saklama
+
         cell.cellProtocol = self
         cell.indexPath = indexPath
-        
+
         return cell
     }
+
+    @objc func imageTapped(_ sender: UITapGestureRecognizer) {
+        let index = sender.view!.tag
+        let food = foodList[index]
+        
+        // Resim tıklandığında yapılacak işlem
+        performSegue(withIdentifier: "toDetail", sender: food)
+    }
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let food = foodList[indexPath.row]
         performSegue(withIdentifier: "toDetail", sender: food)
-        
     }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetail" {
+            if let destinationVC = segue.destination as? Details {
+                if let selectedFood = sender as? Food {
+                    destinationVC.foodList = selectedFood
+                }
+            }
+        }
+    }
+
     func addtoCart(indexPath: IndexPath) {
         let food = foodList[indexPath.row]
         Shared.shared.items.append(food)
-        
     }
-    
 }
